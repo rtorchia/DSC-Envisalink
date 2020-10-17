327fe62d3a95e3b7266679ab59f777722b845485
Ralph Torchia <ralphtorchia1@gmail.com>
2020-10-17 16:07:07 -0400

    change name from flood to water zone

---
+++ devicetypes/rtorchia/dsc-zone-water.src/dsc-zone-water.groovy
@@ -0,0 +1,109 @@
+/*
+ *  DSC Zone Water Device
+ *
+ *  Author: Ralph Torchia
+ *  Originally By: Jordan <jordan@xeron.cc>, Matt Martz <matt.martz@gmail.com>, Kent Holloway <drizit@gmail.com>
+ *  Date: 2020-10-14
+ */
+
+metadata {
+  definition (
+  	name: "DSC Zone Water",
+    author: "Ralph Torchia",
+    namespace: 'rtorchia',
+    mnmn: "SmartThingsCommunity",
+    vid: "b3fd0fa8-6128-3bb9-b166-50104302ecf9"
+  )
+
+  {
+    capability "Water Sensor"
+    capability "Sensor"
+    capability "Alarm"
+    capability "pizzafiber16443.zoneBypass"
+    capability "pizzafiber16443.troubleStatus"
+  }
+
+  tiles {}
+}
+
+// handle commands
+def setZoneBypass() {
+  def zone = device.deviceNetworkId.minus('dsczone')
+  parent.sendUrl("bypass?zone=${zone}")
+  sendEvent (name: "zoneBypass", value: "on")
+}
+
+def zone(String state) {
+  // state will be a valid state for a zone (open, closed)
+  // zone will be a number for the zone
+  log.debug "Zone: ${state}"
+
+  //def troubleList = ['fault','tamper','restore']
+  def troubleMap = [
+    'restore': 'No Trouble',
+    'tamper': 'Tamper',
+    'fault': 'Fault'
+  ]
+
+  def bypassList = ['on','off']
+
+  def alarmMap = [
+    'alarm': "both",
+    'noalarm': "off"
+  ]
+
+  if (troubleMap.containsKey(state)) {
+    sendEvent (name: "trouble", value: "${state}")
+    sendEvent (name: "troubleStatus", value: "${troubleMap[state]}")
+  } else if (bypassList.contains(state)) {
+    sendEvent (name: "bypass", value: "${state}")
+    sendEvent (name: "zoneBypass", value: "${state}")
+  } else {
+    // Send actual alarm state, if we have one
+    if (alarmMap.containsKey(state)) {
+      sendEvent (name: "alarm", value: "${alarmMap[state]}")
+    } else {
+      sendEvent (name: "alarm", value: "off")
+    }
+    // Since this is a water sensor device we need to convert the values to match the device capabilities
+    // Alarming isn't a valid option for this capability, but we map this here anyway, so you can more easily tell which device
+    // is alarming from the "things" page.
+    def waterMap = [
+     'open':"wet",
+     'closed':"dry",
+     'noalarm':"dry",
+     'alarm':"alarm"
+    ]
+
+    sendEvent (name: "water", value: "${waterMap[state]}")
+  }
+}
+
+def updated() {
+  //do nothing for now
+}
+def installed() {
+  initialize()
+}
+
+//just reset if any button is pushed for now
+def both() {
+  sendEvent (name: "alarm", value: "off")
+}
+def off() {
+  sendEvent (name: "alarm", value: "off")
+}
+def siren() {
+  sendEvent (name: "alarm", value: "off")
+}
+def strobe() {
+  sendEvent (name: "alarm", value: "off")
+}
+
+private initialize() {
+  log.trace "Executing initialize()"
+  //set default values
+  sendEvent (name: "troubleStatus", value: "No Trouble")
+  sendEvent (name: "zoneBypass", value: "off")
+  off()
+}
